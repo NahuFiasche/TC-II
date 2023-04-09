e09a999f6ecb75371d533989d6b348e9707b8cf4
Nahu Fiasche <nfiasche@frba.utn.edu.ar>
2023-04-09 13:47:44 -0300

    Transferencia Normalizada

--- Simulaciones/Tarea Semanal 1/Transferencia_Normalizada.py
+++ Simulaciones/Tarea Semanal 1/Transferencia_Normalizada.py
@@ -14,16 +14,20 @@ import numpy as np
 from pytc2.sistemas_lineales import pzmap, GroupDelay, bodePlot
 
 # Asigno valores a R2 y R1 (La consigna no aclara con que valores simular)
-R2 = 1
+R2_lista = [1, 2, 3]
 R1 = 2
 
-Tf = TransferFunction( [1,-R2/R1], [1, 1] )
+
 
 
 plt.close('all')
 
-bodePlot(Tf, fig_id=1, filter_description = 'R1={}K, R2={}K'.format(R1,R2) )
+for i in range(len(R2_lista)):
+    
+    Tf = TransferFunction( [1,-R2_lista[i]/R1], [1, 1] )
 
-pzmap(Tf, fig_id=2, filter_description = 'R1={}K, R2={}K'.format(R1,R2)) #S plane pole/zero plot
+    bodePlot(Tf, fig_id=1, filter_description = 'R2/R1 = {}'.format(R2_lista[i]/R1) )
 
-GroupDelay(Tf, fig_id=3, filter_description = 'R1={}K, R2={}K'.format(R1,R2))
+    pzmap(Tf, fig_id=2, filter_description = 'R2/R1 = {}'.format(R2_lista[i]/R1)) #S plane pole/zero plot
+
+    GroupDelay(Tf, fig_id=3, filter_description = 'R2/R1 = {}'.format(R2_lista[i]/R1))
